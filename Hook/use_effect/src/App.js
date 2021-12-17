68c117f9cefd1521deab8ec52934375ce45201fd
Bach Phung <quangbach19112000@gmail.com>
2021-12-17 09:13:32 +0200

    Push timer useEffect

--- Hook/use_effect/src/App.js
+++ Hook/use_effect/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Content } from "./Content";
 import './App.css'
 function App() {
@@ -14,10 +14,31 @@ function App() {
  *    - Clear timer
  */
   const [show,setShow] = useState(false)
+  const [countdown, setCountDown] = useState(10)
+  const [endCounter,setEndCounter] = useState(false)
+  useEffect(() => {
+    if(countdown === 0){
+      setEndCounter(true)
+      setShow(true)
+      setCountDown(-1)
+    }
+    if(countdown===-1){
+      return
+    }
+    const Counter = setInterval(()=>{
+      console.log('called');
+      setCountDown(countdown - 1)
+    },1000)
+    return () => {
+      window.clearInterval(Counter)
+    }
+  })
   return (
     <div className="App">
+      <h1>{countdown>0?countdown:'0'}</h1>
       <button onClick={()=>setShow(!show)}>Click me!</button>
       <>{show && <Content></Content>}</>
+      
     </div>
   );
 }
