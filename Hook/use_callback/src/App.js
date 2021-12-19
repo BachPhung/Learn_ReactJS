ebde6c2bf6f3e055feed199e49b6abafe1feaef2
Bach Phung <quangbach19112000@gmail.com>
2021-12-19 04:55:08 +0200

    Push

--- Hook/use_callback/src/App.js
+++ Hook/use_callback/src/App.js
@@ -1,18 +1,16 @@
-import {useEffect, useState} from 'react'
+import {useEffect, useState, useCallback} from 'react'
 import './App.css';
 import Content from './Content';
 
 function App() {
   const [count,setCount] = useState(0)
-  const increase = () =>{
-    setCount(count+1)
-  }
+  const handleIncrease = useCallback(() =>{
+    setCount(pre=>pre+1)
+  },[])
   return (
     <div style={{padding: '10px 32px'}}>
       <h1>{count}</h1>
-      <Content></Content>
-      <button onClick={increase}>Click me!</button>
-      
+      <Content onIncrease={handleIncrease}></Content>
     </div>
   );
 }
--- Hook/use_callback/src/Content.js
+++ Hook/use_callback/src/Content.js
@@ -1,9 +1,10 @@
 import { memo } from "react"
-const Content = () => {
+const Content = ({onIncrease}) => {
     console.log('rendered')
     return (
         <div>
             <h2>Hello everybody</h2>
+            <button onClick={onIncrease}>Click me!</button>
         </div>
     )
 }
